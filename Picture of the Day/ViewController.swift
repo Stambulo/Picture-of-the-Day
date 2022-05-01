ca9a8b44cc638da473a21eb5bb5e859f0ee749ba
Anatolii <stambulo@yandex.ru>
2022-05-01 19:44:11 +0300

    Initial Commit

---
+++ Picture of the Day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picture of the Day
+//
+//  Created by User on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
